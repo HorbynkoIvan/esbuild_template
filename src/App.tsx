138b24c76bdca7b0f0e8eb2310ddaab86f87033d
Ivan Gorbynko <ivan.gorbynko@gmail.com>
2022-07-10 12:57:56 +0300

    add img loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import {useState} from "react";
 
+import Gallery from './images/gallery.png';
+
 const App = () => {
-    const [count, setCount] = useState<number>(10)
+    const [count, setCount] = useState<number>(1)
 
     return (
         <>
             <div>Hello world</div>
+            <img src={Gallery} alt=""/>
             <h1>value: {count}</h1>
             <button onClick={() => setCount(count + 1)}>setCount</button>
         </>
